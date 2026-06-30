15ffbdf41c95d0a2fbf7e6baf88fa8ea44cfc236
ErickCJ18 <162001501+ErickCJ18@users.noreply.github.com>
2026-06-29 22:45:31 -0400

    Update Cafetera.java
    Se agregaron los campos privados marca y modelo, así como un nuevo Constructor y nuevos métodos.

--- src/cafe/Cafetera.java
+++ src/cafe/Cafetera.java
@@ -3,10 +3,21 @@ package cafe;
 public class Cafetera
 {
 	private int cantidadCafe;
+	private String marca;
+	private String modelo;
 	
 	public Cafetera(int cantidadCafe) 
 	{
 		this.cantidadCafe = cantidadCafe;
+		this.marca = "Desconocida";
+		this.modelo = "Desconocido";
+	}
+	
+	public Cafetera(int cantidadCafe, String marca, String modelo) 
+	{
+		this.cantidadCafe = cantidadCafe;
+		this.marca = marca;
+		this.modelo = modelo;
 	}
 	
 	void setCantidadDeCafe(int param1) 
@@ -17,6 +28,26 @@ public class Cafetera
 	public int getCantidadDeCafe() 
 	{
 		return this.cantidadCafe;
+	}
+	
+	public void setMarca(String marca)
+	{
+		this.marca = marca;
+	}
+	
+	public String getMarca()
+	{
+		return this.marca;
+	}
+	
+	public void setModelo(String modelo)
+	{
+		this.modelo = modelo;
+	}
+	
+	public String getModelo()
+	{
+		return this.modelo;
 	}
 	
 	public boolean hasCafe(int checkCafe) 
